40f8ce644d080fad18a62bd2f58c9fa50b1f120c
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-09-23 18:13:08 -0300

    Removi o endpoint de teste

--- modulo5/projeto-labook/src/index.ts
+++ modulo5/projeto-labook/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from "dotenv"
-import { pingRouter } from './router/pingRouter'
 import { userRouter } from './router/userRouter'
 import { postRouter } from './router/postRouter'
 
@@ -15,6 +14,5 @@ app.listen(process.env.PORT || 3003, () => {
     console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
 })
 
-app.use("/ping", pingRouter)
 app.use("/users", userRouter)
 app.use("/posts", postRouter)
